1077fda73ad04c2b43ed2d548cf487fa4642b012
Luis Mendoza <luis.mendoza.alonso@hotmail.com>
2022-12-22 17:41:53 -0500

    Se crean los componentes y se servicios a usar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { TarjetaPeliculaComponent } from './components/tarjeta-pelicula/tarjeta-pelicula.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeliculasComponent,
+    TarjetaPeliculaComponent
   ],
   imports: [
     BrowserModule
